b12ef1114463605d06add3bfd885e236f66ea3f3
Bruno Michelini Silva <michelinibrunosilva@gmail.com>
2021-03-16 21:58:09 -0300

    acrescentado o casousel.js (necessario consertar imagem invisivel, botoes prev e next e menu do nav)

--- js/main.js
+++ js/main.js
@@ -8,4 +8,20 @@
         toggleBtn: 'header__btnMenu',
         widthEnabled: 1024
     })
+
+    var carouselImg = new Carousel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+    var carouselQuotes = new Carousel({
+        container: 'quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+
 })()
